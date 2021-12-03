524525a8818733ff0a5d6588bfea30f8714d35a3
Taylan Aydinli <taylanaydinli@gmail.com>
2021-12-03 20:40:03 +0100

    Add solution for day 1

---
+++ day-01/Program.cs
@@ -0,0 +1,16 @@
+﻿
+List<int> measurements = File.ReadAllLines("input.txt").Select(l => int.Parse(l)).ToList();
+
+int numIncreasingMeasurements = 0;
+
+for (int current = 1; current < measurements.Count; current++)
+{
+    int previous = current - 1;
+
+    if (measurements[current] > measurements[previous])
+    {
+        numIncreasingMeasurements++;
+    }
+}
+
+Console.WriteLine(numIncreasingMeasurements);
