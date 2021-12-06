48700b0cf083c28d0b1d49acd275ec3ac0832a53
Taylan Aydinli <taylanaydinli@gmail.com>
2021-12-06 16:25:06 +0100

    Add solution for day 04

--- day-04/Program.cs
+++ day-04/Program.cs
@@ -1,20 +1,26 @@
 ﻿using System.Text;
 
-List<string> lines = File.ReadAllLines("test-input.txt").ToList();
+List<string> lines = File.ReadAllLines("input.txt").ToList();
 
-BingoGame game = new BingoGame(lines);
+BingoGame game = new(lines);
 
-Console.WriteLine(game.ToString());
 
 game.PlayGame();
 
-Console.WriteLine(game.ToString());
+if (game.HasWinner)
+    Console.WriteLine(game.GetFinalScore());
 
+// Console.WriteLine(game);
 
 public class BingoGame
 {
-    private List<int> _numbers;
-    private List<BingoBoard> _boards = new();
+    private readonly List<int> _numbers;
+    private readonly List<BingoBoard> _boards = new();
+    
+    private int _lastPlayedNumber = -1;
+    private BingoBoard _winningBoard = null!;
+
+    public bool HasWinner => this._winningBoard != null;
 
     public BingoGame(List<string> lines)
     {
@@ -24,20 +30,46 @@ public class BingoGame
         int numBoards = lines.Count(s => s == "") + 1;
         for (int i = 0; i < numBoards; i++)
         {
-            BingoBoard board = new();
+            BingoBoard board = new(i + 1);
             board.Parse(lines.Skip(i * 6).Take(5).ToList());
             this._boards.Add(board);
         }
     }
 
+    public int GetFinalScore()
+    {
+        return this._winningBoard.GetSumOfAllUnmarkedNumbers() * this._lastPlayedNumber;
+    }
+
     public void PlayGame()
     {
-        foreach (int number in this._numbers.Take(5))
+        foreach (int number in this._numbers)
         {
             foreach (BingoBoard board in this._boards)
             {
                 board.ProcessNumber(number);
+
+                this._lastPlayedNumber = number;
+
+                if (board.HasBingo)
+                {
+                    this._winningBoard = board;
+                    break;
+                }
             }
+
+            if (this._winningBoard != null)
+                break;
+        }
+        
+        if (this._winningBoard != null)
+        {
+            Console.WriteLine($"WINNING BOARD:");
+            Console.WriteLine(this._winningBoard);
+        }
+        else
+        {
+            Console.WriteLine("NO BINGO FOR YOU");
         }
     }
     
@@ -56,7 +88,7 @@ public class BingoGame
 public class BingoCell
 {
     public int Number { get; internal init; }
-    private bool Marked { get; set; }
+    public bool Marked { get; private set; }
 
     public void Mark()
     {
@@ -71,10 +103,23 @@ public class BingoCell
 
 public class BingoBoard
 {
+    
     private const int BingoBoardSize = 5;
     
     private readonly BingoCell[,] _board = new BingoCell[BingoBoardSize, BingoBoardSize];
     private Dictionary<int, Tuple<int, int>> CellIndex { get; } = new();
+
+    private bool _hasRowBingo = false;
+    private bool _hasColumnBingo = false;
+    private int _bingoRowIndex = -1;
+    private int _bingoColumnIndex = -1;
+
+    public BingoBoard(int id)
+    {
+        this.ID = id;
+    }
+
+    public int ID { get; private set; }
 
     private void SetCell(int i, int j, int value)
     {
@@ -82,7 +127,7 @@ public class BingoBoard
         this.CellIndex[value] = new Tuple<int, int>(i, j);
     }
 
-    public bool HasBingo => false;
+    public bool HasBingo => this._hasRowBingo || this._hasColumnBingo;
 
     public void ProcessNumber(int number)
     {
@@ -91,6 +136,38 @@ public class BingoBoard
         
         (int i, int j) = this.CellIndex[number];
         this._board[i, j].Mark();
+        
+        this.UpdateHasBingo(Tuple.Create(i, j));
+    }
+
+    private void UpdateHasBingo(Tuple<int, int> lastUpdatedCell)
+    {
+        bool hasRowBingo = true;
+        for (int j = 0; j < BingoBoardSize; j++)
+        {
+            hasRowBingo = hasRowBingo && this._board[lastUpdatedCell.Item1, j].Marked;
+        }
+        
+        if (hasRowBingo)
+        {
+            this._hasRowBingo = true;
+            this._bingoRowIndex = lastUpdatedCell.Item1;
+            Console.WriteLine($"Board {this.ID} has bingo on row {this._bingoRowIndex}");
+            return;
+        }
+
+        bool hasColumnBingo = true;
+        for (int i = 0; i < BingoBoardSize; i++)
+        {
+            hasColumnBingo = hasColumnBingo && this._board[i, lastUpdatedCell.Item2].Marked;
+        }
+
+        if (hasColumnBingo)
+        {
+            this._hasColumnBingo = true;
+            this._bingoColumnIndex = lastUpdatedCell.Item2;
+            Console.WriteLine($"Board has bingo on column {this._bingoColumnIndex}");
+        }
     }
 
     public void Parse(List<string> lines)
@@ -119,6 +196,21 @@ public class BingoBoard
         }
 
         return builder.ToString();
+    }
+
+    public int GetSumOfAllUnmarkedNumbers()
+    {
+        int sum = 0;
+        
+        for (int i = 0; i < BingoBoardSize; i++)
+        {
+            for (int j = 0; j < BingoBoardSize; j++)
+            {
+                sum += !this._board[i, j].Marked ? this._board[i, j].Number : 0;
+            }
+        }
+
+        return sum;
     }
 }
 
