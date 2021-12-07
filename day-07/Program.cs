d2c301f292118b413883301c2e4d8c0a6d2c64b1
Taylan Aydinli <taylanaydinli@gmail.com>
2021-12-07 20:09:17 +0100

    Add solution for day 07

---
+++ day-07/Program.cs
@@ -0,0 +1,22 @@
+﻿
+List<int> crabs = File.ReadAllText("input.txt").Split(',').Select(int.Parse).ToList();
+
+int numTargetPositions = crabs.Count;
+
+int minFuelCost = int.MaxValue;
+
+for (int potentialTargetPosition = 0; potentialTargetPosition < numTargetPositions; potentialTargetPosition++)
+{
+    int fuelCost = 0;
+    foreach (int crab in crabs)
+    {
+        fuelCost += Math.Abs(crab - potentialTargetPosition);
+    }
+
+    if (fuelCost < minFuelCost)
+    {
+        minFuelCost = fuelCost;
+    }
+}
+
+Console.WriteLine(minFuelCost);
