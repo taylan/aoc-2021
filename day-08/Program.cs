842426b403e2f82065aaddaae324b20e0fdf40c1
Taylan Aydinli <taylanaydinli@gmail.com>
2021-12-08 15:35:06 +0100

    Add solution for day 8 level 1

---
+++ day-08/Program.cs
@@ -0,0 +1,15 @@
+﻿
+List<string> lines = File.ReadAllLines("input.txt").ToList();
+
+int sum = 0;
+
+List<int> acceptableLengths = new() {2, 4, 3, 7};
+
+foreach (string line in lines)
+{
+    int count = line.Split('|')[1].Trim().Split(' ').Count(s => acceptableLengths.Contains(s.Length));
+
+    sum += count;
+}
+
+Console.WriteLine(sum);
